d99c03ec8e408bf3c02c7773e4df2e8c84b60a9b
Jamyth <jamyth.luk@okg.com>
2022-06-16 23:14:38 +0800

    [REFACTOR]: confiure build

--- script/build.ts
+++ script/build.ts
@@ -8,16 +8,27 @@ const logger = createConsoleLogger("PKGRoll Compiler");
 require("./format");
 require("./lint");
 
+const FilePath = {
+    src: path.join(__dirname, "../src"),
+    dist: path.join(__dirname, "../dist"),
+};
+
 function build() {
     logger.task("Build and Transpile");
     spawn("tsc", ["--project", path.join(__dirname, "../config/tsconfig.src.json")], "Build Failed.");
 }
 
 function copyTemplate() {
-    const location = path.resolve(__dirname, "../src/template");
-    const target = path.resolve(__dirname, "../dist/template");
+    const location = path.resolve(FilePath.src, "template");
+    const target = path.resolve(FilePath.dist, "template");
     fs.copySync(location, target, { recursive: true });
+}
+
+function copyAsset() {
+    fs.copyFileSync(path.join(__dirname, "../package.json"), path.join(FilePath.dist, "package.json"));
+    fs.copyFileSync(path.join(__dirname, "../LICENSE"), path.join(FilePath.dist, "LICENSE"));
 }
 
 build();
 copyTemplate();
+copyAsset();
